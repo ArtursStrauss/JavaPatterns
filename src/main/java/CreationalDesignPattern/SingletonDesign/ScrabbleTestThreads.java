dbaca5e843933b3cb999b0eb29fc66d2c8cc846c
artustra <arturs.strauss@gmail.com>
2017-09-22 00:22:15 +0300

    Singleton pattern.

---
+++ src/main/java/CreationalDesignPattern/SingletonDesign/ScrabbleTestThreads.java
@@ -0,0 +1,10 @@
+package CreationalDesignPattern.SingletonDesign;
+
+public class ScrabbleTestThreads {
+    public static void main(String[] args) {
+        Runnable getTiles = new GetTheTiles();
+        Runnable getTilesAgain = new GetTheTiles();
+        new Thread(getTiles).start();
+        new Thread(getTilesAgain).start();
+    }
+}
